e0be9ef7c5ae520e4098c93ea4a9f476ea877d3a
Renan <renanmagalhaesreis@gmail.com>
2024-08-07 19:40:05 -0300

    classe model de solicitacao criada

--- salefrut/Solicitacao/models.py
+++ salefrut/Solicitacao/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from salefrut import Produto, Usuario
+
+class Solicitacao(models.Model):
+    # ForengnKey: chave estrangeira de produto
+    # on_delete=models.CASCADE: se o produto for excluido as  todas as solicitações associadas a esse produto também sejam excluídas automaticamente
+    # related_name='solicitacoes: permite acessar todas as solicitações feitas por um usuário usando     "usuario.solicitacoes.all()."
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    solicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='solicitacoes')
+    data_solicitacao = models.DateTimeField(auto_now_add=True)
+    quantidade = models.IntegerField()

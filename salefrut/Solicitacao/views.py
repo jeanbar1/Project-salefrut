d7ae69ae8b238550ea7c9ce26f050588dcc5a184
Renan <renanmagalhaesreis@gmail.com>
2024-08-07 20:10:44 -0300

    primeiro commit

--- salefrut/Solicitacao/views.py
+++ salefrut/Solicitacao/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from salefrut import Produto
+from salefrut.Solicitacao.forms import SolicitacaoForm
 
 # Create your views here.
+def solicitar_produto(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id)
+    if request.method == 'POST':
+        form = SolicitacaoForm(request.POST)
+        if form.is_valid():
+            solicitacao = form.save(commit=False)
+            solicitacao.produto = produto
+            solicitacao.solicitante = request.user
+            solicitacao.save()
+            return redirect('lista_produtos')
+    else:
+        form = SolicitacaoForm()
+    return render(request, 'marketplace/solicitar_produto.html', {'form': form, 'produto': produto})
